8849f1de0c98ae4183f9c219f675d38d1795521b
vlothec <64958833+vlothec@users.noreply.github.com>
2023-03-10 09:55:41 +0100

    Update fn_new_distance_N.R

--- src/fn_new_distance_N.R
+++ src/fn_new_distance_N.R
@@ -9,33 +9,32 @@ new.distance.N = function(distances, N.max.div = 100, try.until = 12, smooth.per
   
   N = dist.hist$breaks[which.max(dist.hist$counts) + 1]
   
-  if(T)
+  hist.smooth.range = ceiling(N*smooth.percent/100)
+  
+  new.values = vector(mode = "numeric", length = try.until)
+  
+  for(i in 1 : try.until)
   {
-    hist.smooth.range = ceiling(N*smooth.percent/100)
+    new.values[i] = sum(dist.hist$counts[dist.hist$breaks %in% (ceiling((N-hist.smooth.range)/i) : ceiling((N+hist.smooth.range)/i)) ])
+  }
+  scores.with.multiplications = vector(mode = "numeric", length = try.until)
+  for(i in 1 : try.until)
+  {
+    new.N = round(N/i)
+    new.value.score = new.values[i]
+    possible.multiplications = 1:i
+    scores.with.multiplications[i] = sum(new.values[possible.multiplications])
     
-    new.values = vector(mode = "numeric", length = try.until)
-    
-    for(i in 1 : try.until)
-    {
-      new.values[i] = sum(dist.hist$counts[dist.hist$breaks %in% (ceiling((N-hist.smooth.range)/i) : ceiling((N+hist.smooth.range)/i)) ])
-    }
-    scores.with.multiplications = vector(mode = "numeric", length = try.until)
-    for(i in 1 : try.until)
-    {
-      new.N = round(N/i)
-      new.value.score = new.values[i]
-      possible.multiplications = 1:i
-      scores.with.multiplications[i] = sum(new.values[possible.multiplications])
-      
-    }
-    plot(scores.with.multiplications, ylim = c(0, max(scores.with.multiplications)))
-    
-    options(scipen=999)
-    scores.ratio = 100 * (1 - (scores.with.multiplications[1:(try.until-1)] / scores.with.multiplications[2:try.until]))
-    
+  }
+  plot(scores.with.multiplications, ylim = c(0, max(scores.with.multiplications)))
+  
+  options(scipen=999)
+  scores.ratio = 100 * (1 - (scores.with.multiplications[1:(try.until-1)] / scores.with.multiplications[2:try.until]))
+  
+  if(length(which(scores.ratio > N.max.div)) > 0)
+  {
     N = N / round(max(which(scores.ratio > N.max.div)) + 1)
   }
-  
   
   return(N)
 }
